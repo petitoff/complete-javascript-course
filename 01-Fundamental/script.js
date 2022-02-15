4b93e728829f340f3fe19fa7757f35d42590e4c1
petitoff <petitoff@outlook.com>
2022-02-15 16:36:37 +0100

    Update script.js

--- 01-Fundamental/script.js
+++ 01-Fundamental/script.js
@@ -123,3 +123,10 @@ console.log(typeof NaN);
 console.log(String(23), 23);
 
 //type coersion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
