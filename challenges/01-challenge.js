3b28fd6a8652074821c7ef10e687433c47e8333a
petitoff <petitoff@outlook.com>
2022-02-15 15:38:32 +0100

    Update 01-challenge.js

--- challenges/01-challenge.js
+++ challenges/01-challenge.js
@@ -1,21 +1,12 @@
-let wagaMarka = 78;
-let wagaJohna = 92;
+let wagaMarka = 95;
+let wagaJohna = 85;
 
-let wzrostMarka = 1.69;
-let wzrostJohna = 1.95;
+let wzrostMarka = 1.88;
+let wzrostJohna = 1.75;
 
 let markBMI = wagaMarka / wzrostMarka ** 2;
 let johnBMI = wagaMarka / wzrostMarka ** 2;
 
-let markHigherBMI;
-
-if (markBMI > johnBMI)
-{
-    markHigherBMI = true;
-}
-else
-{
-    markHigherBMI = false;
-}
+let markHigherBMI = markBMI > johnBMI;
 
 console.log(markHigherBMI);
