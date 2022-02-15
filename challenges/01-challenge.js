c3e03d7e2f2b967a866ca59014821c01b965e524
petitoff <petitoff@outlook.com>
2022-02-15 15:36:23 +0100

    Update 01-challenge.js

--- challenges/01-challenge.js
+++ challenges/01-challenge.js
@@ -4,8 +4,8 @@ let wagaJohna = 92;
 let wzrostMarka = 1.69;
 let wzrostJohna = 1.95;
 
-let markBMI = (wagaMarka / wzrostMarka) ** 2;
-let johnBMI = (wagaMarka / wzrostMarka) ** 2;
+let markBMI = wagaMarka / wzrostMarka ** 2;
+let johnBMI = wagaMarka / wzrostMarka ** 2;
 
 let markHigherBMI;
 
