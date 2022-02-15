51b2942fc87283562e3acc6a33b8030c0908dcb1
petitoff <petitoff@outlook.com>
2022-02-15 16:07:03 +0100

    Update script.js

--- 01-Fundamental/script.js
+++ 01-Fundamental/script.js
@@ -99,3 +99,13 @@ else {
     const yearsLeft = 18 - age;
     console.log(`Sarah is too young. Wait another ${yearsLeft} years!`)
 }
+
+const birthYear = 1991;
+let century;
+if(birthYear <= 2000) {
+    century = 20;
+}
+else {
+    century = 21;
+}
+console.log(century);
