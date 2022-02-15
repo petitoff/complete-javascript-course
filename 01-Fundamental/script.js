677573f383267cb0a5c5536a368e570f547ccfb3
petitoff <petitoff@outlook.com>
2022-02-15 16:00:24 +0100

    Update script.js

--- 01-Fundamental/script.js
+++ 01-Fundamental/script.js
@@ -90,3 +90,12 @@ lines`)
 
 */
 
+const age = 15;
+
+if(age >= 18) {
+    console.log('Sarah can start driving license ❤')
+}
+else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years!`)
+}
