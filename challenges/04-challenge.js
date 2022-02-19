ceee2c572880a36450ef4f2775b6d8b2509d7093
petitoff <petitoff@outlook.com>
2022-02-19 21:55:38 +0100

    Update 04-challenge.js

--- challenges/04-challenge.js
+++ challenges/04-challenge.js
@@ -1,4 +1,4 @@
 const bill = 275.40;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15: bill * 0.2
 
-console.log(`Your bill is ${tip.toFixed(2)}`);
+console.log(`Your tip is ${tip.toFixed(2)}`);
