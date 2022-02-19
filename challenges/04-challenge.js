2f43c1b9f2d1ef3a3212c04120d1dadd611b127a
petitoff <petitoff@outlook.com>
2022-02-19 21:51:28 +0100

    Update 04-challenge.js

--- challenges/04-challenge.js
+++ challenges/04-challenge.js
@@ -1,3 +1,3 @@
 const tip = 275.40;
 
-console.log(`Your tip is ${tip >= 50 || tip <= 300 ? tip * 0.15: tip * 0.20}`);
+console.log(`Your tip is ${tip >= 50 && tip <= 300 ? tip * 0.15: tip * 0.20}`);
