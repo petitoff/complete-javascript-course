c3b5438dd000b1cb79cfbbbcfd3ef3bf33bbb9b3
petitoff <petitoff@outlook.com>
2022-02-19 19:58:37 +0100

    case

--- 01-Fundamental/script.js
+++ 01-Fundamental/script.js
@@ -156,3 +156,27 @@ console.log(hasDriversLicense && hasGoodVision);
 console.log(hasDriversLicense || hasGoodVision);
 
 */
+
+const day = 'tuesday';
+switch(day){
+    case 'monday':  // day === 'monday'
+        console.log('Plan course structure.');
+        console.log('Go to coding meetup.')
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos.');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples.');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
