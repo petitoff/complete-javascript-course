3679f3d2c718d5bf116a120d761781e7a2db1d4a
petitoff <petitoff@outlook.com>
2022-02-21 14:40:28 +0100

    Update script.js

--- 02-Fundamental/script.js
+++ 02-Fundamental/script.js
@@ -84,6 +84,7 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(2, 3));
 */
 
+/*
 const calcAge = function (birthYear) {
   return 2037 - birthYear;
 };
@@ -104,3 +105,4 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 };
 console.log(yearsUntilRetirement(1991, "Jonas"));
 console.log(yearsUntilRetirement(1950, "Mike"));
+*/
