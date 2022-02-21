c4f6695bfe4456bba563144ff330f17784bd4113
petitoff <petitoff@outlook.com>
2022-02-21 13:32:02 +0100

    Update script.js

--- 02-Fundamental/script.js
+++ 02-Fundamental/script.js
@@ -31,6 +31,7 @@ const appleJuice = fruitProcessor(12, 15);
 console.log(appleJuice);
 */
 
+/*
 // Function declaration
 const age11 = calcAge1(1991); // It will work
 
@@ -50,3 +51,21 @@ const calcAge2 = function (birtYear) {
 
 const age2 = calcAge2(1991);
 console.log(age2);
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+
+  if (retirement > 0) return `${firstName} retires in ${retirement} years`;
+  else return `${firstName} is retired!`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1950, "Bob"));
+*/
