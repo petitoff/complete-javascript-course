71a186724c2fbaa65f361662ba777f02afc82560
petitoff <petitoff@outlook.com>
2022-02-22 12:27:49 +0100

    Dot vs. Bracket Notation

--- 02-Fundamental/script.js
+++ 02-Fundamental/script.js
@@ -180,12 +180,39 @@ if (friends.includes("Peter")) {
 }
 */
 
-const jonasArray = ["Jonas", "Schmedtmann"];
-
 const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
   age: 2037 - 1991,
   job: "teacher",
-  friends: ["Michael", "Peter"],
+  friends: ["Michael", "Peter", "Steven"],
 };
+
+// console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, friends"
+);
+
+// console.log(jonas.interestedIn); // This will not work
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Wrong request!");
+}
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtman";
+console.log(jonas);
+
+//Challenge
+// "Jonas has 3 friends and his best friend is called Michael"
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`
+);
