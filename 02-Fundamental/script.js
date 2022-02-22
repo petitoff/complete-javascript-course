456cbe99bcf2c0f6ed0ea7d67c541e7dcb0e245d
petitoff <petitoff@outlook.com>
2022-02-22 16:22:35 +0100

    The while Loop

--- 02-Fundamental/script.js
+++ 02-Fundamental/script.js
@@ -358,3 +358,18 @@ for (let exercise = 1; exercise <= 3; exercise++) {
   }
 }
 */
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weight repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Exiting...");
+}
