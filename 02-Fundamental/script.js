8d64397612b0b30b621f0d930aeeb017a9eb9c65
petitoff <petitoff@outlook.com>
2022-02-22 12:57:34 +0100

    Object Methods

--- 02-Fundamental/script.js
+++ 02-Fundamental/script.js
@@ -218,3 +218,50 @@ console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`
 );
 */
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+
+  // calcAge: function () {
+  //   return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-years old ${
+      this.job
+    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+// console.log(jonas["calcAge"](1991));
+
+// Challenge
+// "Jonas is a 46-years old teacher, and he has a/no driver's license."
+
+// My solution
+console.log(
+  `${jonas.firstName} is a ${jonas.age}-years old ${jonas.job}, and he has ${
+    jonas.hasDriversLicense ? "a" : "no"
+  } driver's license.`
+);
+
+// Jonas solution
+console.log(jonas.getSummary());
