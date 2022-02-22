f2ed52d2e5b76bb4a596aaa8025a26e96b494309
petitoff <petitoff@outlook.com>
2022-02-22 16:01:05 +0100

    Looping Backwards and Loops in Loops

--- 02-Fundamental/script.js
+++ 02-Fundamental/script.js
@@ -336,3 +336,23 @@ for (let i = 0; i < jonas.length; i++) {
   console.log(jonas[i], typeof jonas[i]);
 }
 */
+
+const jonas = [
+  "Jonas",
+  "Schnedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+  console.log(`----- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep <= 5; rep++) {
+    console.log(`Lifting weight repetition ${rep}`);
+  }
+}
