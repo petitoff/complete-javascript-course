11f1018b80afc28152187114fe73e480c98d4dde
petitoff <petitoff@outlook.com>
2022-02-23 17:34:20 +0100

    Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 // Selecting elements
+const playerElement0 = document.querySelector(".player--0");
+const playerElement1 = document.querySelector(".player--1");
+
 const scoreElement0 = document.querySelector("#score--0");
 const scoreElement1 = document.getElementById("score--1");
 const currentElement0 = document.getElementById("current--0");
@@ -16,7 +19,18 @@ scoreElement0.textContent = 0;
 scoreElement1.textContent = 0;
 diceElement.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+const changeCurrentScore = function (str) {
+  document.getElementById(`current--${activePlayer}`).textContent = str;
+};
+
+const changeActivePLayer = function () {
+  playerElement0.classList.toggle("player--active");
+  playerElement1.classList.toggle("player--active");
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -31,8 +45,14 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    currentElement0.textContent = currentScore; // ! Change later
+    changeCurrentScore(currentScore);
+
+    // currentElement0.textContent = currentScore; // ! Change later
   } else {
     // switch to next player
+    changeCurrentScore(0);
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    changeActivePLayer();
   }
 });
