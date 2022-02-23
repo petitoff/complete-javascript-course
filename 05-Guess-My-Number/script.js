0bd8ff944c88cf30b9f124ad6b6869f6b241a11c
petitoff <petitoff@outlook.com>
2022-02-23 12:24:17 +0100

    Update script.js

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "Correct Number!";
 
@@ -8,3 +9,4 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
