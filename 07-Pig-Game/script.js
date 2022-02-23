63db2d435367e417cfcb971d9d69035281e80dde
petitoff <petitoff@outlook.com>
2022-02-23 16:30:35 +0100

    PROJECT #3: Pig Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Selecting elements
+const scoreElement0 = document.querySelector("#score--0");
+const scoreElement1 = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+// Starting conditions
+scoreElement0.textContent = 0;
+scoreElement1.textContent = 0;
+diceElement.classList.add("hidden");
