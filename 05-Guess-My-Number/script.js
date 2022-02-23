0965a7220ac400ead5153b7a1facee416afc8324
petitoff <petitoff@outlook.com>
2022-02-23 12:38:28 +0100

    Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,3 +10,12 @@ document.querySelector(".score").textContent = 10;
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+  }
+});
