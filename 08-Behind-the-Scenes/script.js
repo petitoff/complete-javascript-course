5488185f5842a8982a39f530f2487b39d67569bc
Błażej Domagała <petitoff@outlook.com>
2022-02-26 16:02:26 +0100

    The this Keyword in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -78,3 +78,36 @@ console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
 */
+
+/*
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+*/
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+console.log(f);
+f();
