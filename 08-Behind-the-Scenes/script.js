3cece17783f769006364c414dcbea2a6f08b8590
Błażej Domagała <petitoff@outlook.com>
2022-02-27 17:07:14 +0100

    Primitives vs. Objects
    (Primitive vs. Reference Types)

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -159,3 +159,18 @@ var addArrow = (a, b) => {
 };
 addArrow(2, 5, 8);
 */
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: "Jonas",
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log("Me", me);
+console.log("Friend", friend);
