5a69a4f219256234945cbfd4ca5faeac7282974d
Błażej Domagała <petitoff@outlook.com>
2022-03-13 15:47:42 +0100

    107. Short Circuiting (&& and ||)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,34 @@ const restaurant = {
   },
 };
 
+console.log("---- OR ----");
+console.log(3 || "Konas");
+console.log("" || "Jonas");
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || "" || "Hello" || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+console.log("---- AND ----");
+console.log(0 && "Jonas");
+console.log(7 && "Jonas");
+
+console.log("Hello" && 23 && null && "jonas");
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza("mushrooms", "spinach");
+}
+
+restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+
 /*
 // 106. Rest Pattern and Parameters
 // ! 1) Destructuring
