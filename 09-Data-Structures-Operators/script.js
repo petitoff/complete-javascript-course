98360cdf6ec0fec79aa3a4ae37e34e1e37ba17a3
Błażej Domagała <petitoff@outlook.com>
2022-03-15 14:27:43 +0100

    111. Looping Arrays: The for-of Loop

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,16 @@ const restaurant = {
   },
 };
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1} : ${el}`);
+}
+
+// console.log(...menu.entries());
+
 /*
 // 109. Logical Assignment Operators
 const rest1 = {
