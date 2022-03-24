90ead0ed9b1928c256e4547996461c3221bbc180
Błażej Domagała <petitoff@outlook.com>
2022-03-24 14:06:14 +0000

    114. Looping Objects: Object Keys, Values, and Entries

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,28 @@ const restaurant = {
   },
 };
 
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
 /*
 // 113. Optional Chaining (?.)
 if (restaurant.openingHours && restaurant.openingHours.mon) {
